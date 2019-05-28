84a04fc32280674bc7e7048fe6b5a158457efe35
Alejandro Salazar <ale_salazar@outlook.com>
2019-05-27 20:30:57 -0400

    Removed outdated comments, improved the design, implemented intro screen to ask for single player or multiplayer

--- scripts.js
+++ scripts.js
@@ -1,14 +1,10 @@
-//Declarando variables
-let cells, remainingCells, player;
+var cells, player, aion;
 
-//Inicializando variables
 const board = document.getElementById("board");
-player=1;
+player = 1;
 cells = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
 
-// Checar si un mismo jugador marcó todas las celdas de una misma columna
 function checkColumn(column){
-  //Esto es sólo para hacerme la vida más fácil en vez de escribir cells.get(getCell([1,2,3],column)) cuatro veces
   let result = true;
 
   for(let i = 0; i < 2; i++) {
@@ -18,13 +14,11 @@ function checkColumn(column){
     }
   }
 
-  //Si el mismo jugador marcó todas las celdas de la columna, devuelve true
   return result
 }
 
-// Checar si un mismo jugador marcó todas las celdas de una misma fila
 function checkRow(row){
-  //Esto es sólo para hacerme la vida más fácil en vez de escribir cells.get(getCell(row, [1,2,3])) cuatro veces
+  
   let result = true;
 
   for(let i = 0; i < 2; i++){
@@ -34,65 +28,58 @@ function checkRow(row){
     }
   }
   
-  //Si el mismo jugador marcó todas las celdas de la fila, devuelve true
   return result;
 }
 
-//Checar si un mismo jugador marcó todas las celdas de una diagonal
 function checkDiagonal(row, column){
-  //Si la fila y la columna son iguales, significa que la celda está en la diagonal principal
+  
   if(row === column){
-    //Checamos si un jugador marcó todas las celdas de esa diagonal
+    
     if(cells[0][0] === cells[1][1] && cells[1][1] === cells[2][2]){
       return true;
     }
   }
-  //Si el valor absoluto de la resta de la fila y la columna da 2, o se trata de la celda 22, la celda es parte de la diagonal secundaria
+  
   else if(row + column === 2){
-    //Checamos si un jugador marcó todas las celdas de esa diagonal
-    if(cells[0][2] == cells[1][1] && cells[1][1] == cells[2][0]){
+    
+    if(cells[0][2] === cells[1][1] && cells[1][1] === cells[2][0]){
       return true;
     }
   }
-  //Devolvemos falso en cualquier otro caso
+  
   return false;
 
 }
 
-//Checamos si un jugador ganó
+
 function checkWin(row, column){
 
-  //Se cumplen alguna de las condiciones para ganar?
   return checkDiagonal(row, column) || checkRow(row) || checkColumn(column);
 }
 
 function play(row, column){
-  //La celda aún no ha sido seleccionada?
+  
   if(cells[row][column] === 0){
-    //Le asignamos la celda al jugador
+    
     cells[row][column] = player;
-    //Ponemos una X o una O, según sea el caso
-    document.querySelector(`[data-row="${row}"][data-column="${column}"]`).classList.add(player==1 ? 'x' : 'o');
-    //Alguien ganó?
+    
+    document.querySelector(`[data-row="${row}"][data-column="${column}"]`).classList.add(player === 1 ? 'x' : 'o');
+    
     if(checkWin(row, column)){
-      //Mostramos quién ganó
-      document.getElementById('body').innerHTML += `Player ${player} wins`;
-      //Eliminamos el listener
+      
+      document.querySelector('body').innerHTML += `Player ${player} wins`;
+      
       board.removeEventListener("click", humanPlay);
       return false;
     }
-    //Cambiamos de jugador
-    player = player==1 ? 2 : 1;
+    
+    player = player === 1 ? 2 : 1;
   }
   
 }
 
 function computerPlay(){
-  //Elige una celda al azar entre las disponibles
-  //let cell = remainingCells[Math.round(Math.random() * (remainingCells.length-1))];
-  //let row = getRow(cell);
-  //let column = getColumn(cell);
-
+  
   for(let i = 0; i < 3; i++){
 
     let searching = -1, empty = -1, count = 0;
@@ -221,18 +208,41 @@ function computerPlay(){
 }
 
 function humanPlay(ev){
-  //Si es su turno, juega
-  if(player==1){
+  
+  if ( (aion && player === 1) || !aion ) {
 
     let row = Number(ev.target.attributes["data-row"].value);
     let column = Number(ev.target.attributes["data-column"].value);
 
     play(row, column);
   }
-  //Si hizo una jugada correcta, ahora juega la máquina
-  if(player==2){
-    setTimeout(computerPlay, 500);
+
+  if (aion && player === 2) {
+
+    computerPlay();
+
   }
 }
 
-board.addEventListener("click", humanPlay);
+
+function init (ev) {
+
+  aion = Number(ev.target.attributes["data-aion"].value);
+
+  document.getElementById("intro").remove();
+
+  console.log(board);
+
+  board.style.display = "grid";
+
+  board.addEventListener("click", humanPlay);
+
+}
+
+window.addEventListener('load', () => {
+
+  buttons = document.querySelectorAll("[data-aion]");
+
+  buttons.forEach(button => button.addEventListener('click', init));
+
+});
